9acb33ee1081b2e8271be954471ae5b36d8f39d4
jiayi.xu <jiayi.xu@ipiaoniu.com>
2016-10-04 21:59:48 +0800

    add createUserWithThirdPartyUser

--- cloud.js
+++ cloud.js
@@ -7,7 +7,42 @@ AV.Cloud.define('hello', function(request, response) {
   response.success('Hello world!');
 });
 
-AV.Cloud.define('thirdPartyUserExists', function(request, response) {
+AV.Cloud.define('createUserWithThirdPartyUser', function(request, response) {
+  var thirdPartyUser = request.params.thirdPartyUser
+  var username = request.params.username
+  var password = request.params.password
+
+  if (!thirdPartyUser) {
+    response.error('missing thirdPartyUser')
+  }
+
+  if (!thirdPartyUser.id) {
+    response.error('missing thirdPartyUser.id')
+  }
+
+  if (!thirdPartyUser.platform) {
+    response.error('missing thirdPartyUser.accessKey')
+  }
+
+  if (!thirdPartyUser.accessKey) {
+    response.error('missing thirdPartyUser.accessKey')
+  }
+
+  var attrs = {}
+  attrs[thirdPartyUser.platform + 'Id'] = thirdPartyUser.id
+  attrs[thirdPartyUser.platform + 'AccessToken'] = thirdPartyUser.accessToken
+  attrs[thirdPartyUser.platform + 'Binded'] = 1
+  attrs[name] = username
+
+  AV.User.signUp(username, password, attrs)
+    .then((user) => {
+      response.success(user)
+    }).catch((err) => {
+      response.error(err)
+    })
+})
+
+AV.Cloud.define('getThirdPartyUser', function(request, response) {
   var query = new AV.Query('_User')
   var platformKey = (request.params.platform || 'weibo') + 'Id'
   query.equalTo(platformKey, request.params.id || 0).first().then(function(result){
